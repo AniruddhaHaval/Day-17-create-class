b506888b160349334ca5f16612a110973159a2bc
Aniruddha Haval <havalaniruddha@gmail.com>
2023-05-28 20:10:19 +0530

    Complete creation of Class User

---
+++ main.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+user_1 = User("001", "Aniruddha")
+user_2 = User("002", "Avanti")
+
+print(user_1.followers)
